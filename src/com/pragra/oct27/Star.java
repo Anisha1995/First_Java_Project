06b989f8d56e3f92d32f8603e03eaaf44e4356ed
Anisha1995 <anisha2joy@gmail.com>
2022-10-29 11:15:04 -0400

    programs

---
+++ src/com/pragra/oct27/Star.java
@@ -0,0 +1,17 @@
+package com.pragra.oct27;
+
+
+public class Star {
+    public static void main(String[] args) {
+        for (int i=1;i<=4;i++)
+        {
+            for (int j=1; j<=3;j++)
+            {
+                System.out.print( "* ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
---
+++ src/com/pragra/oct29/program1.java
@@ -0,0 +1,13 @@
+package com.pragra.oct29;
+
+public class program1 {
+    public static void main(String[] args) {
+
+        int couter=0;
+        while (couter<=10)
+        {
+            System.out.println("hello");
+            couter++;
+        }
+    }
+}
